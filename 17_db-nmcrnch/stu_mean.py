a91b6c1cb323c15117d27025d21a517bcc439f07
zane0916 <ZaneWang@Zanes-MacBook-Pro.local>
2018-10-09 01:03:55 -0400

    updated hw

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -24,7 +24,6 @@ def searchGrades():
 #calc avg of students, makes a list of students and their corresponding ids
 def calcAvg():
     idList = []
-    markList = []
     avgList = []
 
     #list of all ids in idList
@@ -33,44 +32,17 @@ def calcAvg():
     for row in c:
         idList.append(row[0])
 
-    print("ALL THE IDS")
-    for id in idList:
-        print(id)
-
-    #list of all marks per id
-    command = "SELECT {0},{1} FROM {2},{3} WHERE {4} = {5}".format("peeps_info.id","mark","peeps_info","courses_info","peeps_info.id","courses_info.id")
-    c.execute(command)
-    print("ALL THE SELECTED ROWS IN C")
-    for row in c:
-        print(row)
-    for id in idList:
-        for row in c:
-            idMark = []
-            if row[0] == id:
-                print(row[1])
-                idMark.append(row[1])
-        markList.append([idMark])
-
-    print("ALL THE MARKS IN MARKLIST")
-    for mark in markList:
-        print(mark)
-
-'''    #returns a list of ids and avgs of students
-    command = "SELECT {0},{1},{2} FROM {3},{4} WHERE {5} = {6}".format("peeps_info.id","name","mark","peeps_info","courses_info","peeps_info.id","courses_info.id")
+    #returns a list of ids and avgs of students
+    command = "SELECT {0},{1},{2} FROM {3},{4} WHERE {5} = {6}".format("peeps_info.id","name","mark","peeps_info","courses_info","peeps_info.id","courses_info.id") 
     c.execute(command)
     for id in idList:
-        num = 0
-        classes = 0
+        totalscore = []
         for row in c:
             if row[0] == id:
-                num += row[2]
-                classes += 1
-        avgList.append([id, num/classes])
+                totalscore.append(row[2])
+        print(num)
+        avgList.append([id, sum(totalscore)/len(totalscore)])
     return avgList
-
-# sum()/len()
-#
-   '''
 
 #calcs and inputs the averages into a table consisting of a student with their corresponding average
 def makeAvgTable():
