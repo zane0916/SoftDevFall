19dec32b0e9440c67c825bacae714dd97f8ed585
zane0916 <zwang7@stuy.edu>
2018-11-13 20:13:12 -0500

    updated hw24

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -4,13 +4,11 @@
 #2018-11-13
 
 from flask import Flask, render_template
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def inform_user():
-    return render_template("index.html", text = "Go to the relative route /nasa")
+    return render_template("index.html", text = '<h1> New Adventures in RESTful APIs: </h1> <img src="https://earthengine.googleapis.com/api/thumb?thumbid=0d15ad8d2af6d8d0a6b083672514fae7&token=28818cafce7b74f5a6951b6051edd969">')
 
-@app.route("/nasa")
-def api_render():
-    return render_template("index.html", text = "<h1> New Adventures in RESTful APIs: </h1><img src=''>")
-    
+if (__name__ == "__main__"):
+    app.run()
