b075ca03e0b5a6dbabfeb4743b20dab548b3004e
zane0916 <zwang7@stuy.edu>
2018-12-18 20:18:09 -0500

    uploaded hw28

---
+++ 28_js/funky_town.js
@@ -0,0 +1,27 @@
+var fibonacci = function(n) {
+    if (n == 0) {
+        return 0;
+    } else if (n <= 2) {
+        return 1;
+    } else {
+        return fibonacci(n-2) + fibonacci(n-1);
+    };
+};
+
+var gcd = function(a, b) {
+    if (a < b) {
+        return gcd(b, a);
+    } else if (b == 0) {
+        return a;
+    } else if (a == 0) {
+        return 0;
+    } else {
+        return gcd(b, a % b);
+    };
+};
+
+var listName = ["Hui Min", "Zane", "Johnny"];
+
+var randomStudent = function() {
+    return listName[Math.floor(Math.random() * listName.length)];
+}
